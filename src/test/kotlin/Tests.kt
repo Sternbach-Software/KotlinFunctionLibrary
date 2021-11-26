30da9e39f09d70556c16d86252284b07f4eacf5c
shmueldabomb441 <shmuelsternbach613@gmail.com>
2021-11-25 19:15:11 -0500

    Fix test cases

--- src/test/kotlin/Tests.kt
+++ src/test/kotlin/Tests.kt
@@ -9,36 +9,36 @@ class Tests {
     fun test_format_time() {
 
         //0, >1, >1
-        Triple(0, 12, 34).formatted(true).println()     //0, >10, >10
-        Triple(0, 12, 3).formatted(true).println()      //0, >10, <10
-        Triple(0, 1, 2).formatted(true).println()       //0, <10, <10
-        Triple(0, 1, 23).formatted(true).println()      //0, <10, >10
+        Triple(0, 12, 34).formatted(true).let { assert(it == "12:34") }     //0, >10, >10
+        Triple(0, 12, 3).formatted(true).let { assert(it == "12:03") }      //0, >10, <10
+        Triple(0, 1, 2).formatted(true).let { assert(it == "1:02") }       //0, <10, <10
+        Triple(0, 1, 23).formatted(true).let { assert(it == "1:23") }      //0, <10, >10
 
         //0, 0, >1
-        Triple(0, 0, 12).formatted(true).println()      //0, 0, >10
-        Triple(0, 0, 1).formatted(true).println()       //0, 0, <10
+        Triple(0, 0, 12).formatted(true).let { assert(it == "00:12") }      //0, 0, >10
+        Triple(0, 0, 1).formatted(true).let { assert(it == "00:01") }       //0, 0, <10
 
         //0, 0, 0
-        Triple(0, 0, 0).formatted(true).println()       //0, 0, 0
+        Triple(0, 0, 0).formatted(true).let { assert(it == "00:00") }       //0, 0, 0
 
         //>1, 0, 0
-        Triple(12, 0, 0).formatted(true).println()      //>10, 0, 0
-        Triple(1, 0, 0).formatted(true).println()       //<10, 0, 0
+        Triple(12, 0, 0).formatted(true).let { assert(it == "12:00:00") }      //>10, 0, 0
+        Triple(1, 0, 0).formatted(true).let { assert(it == "1:00:00") }       //<10, 0, 0
 
         //>1, >1, 0
-        Triple(12, 34, 0).formatted(true).println()     //>10, >10, 0
-        Triple(12, 3, 0).formatted(true).println()      //>10, <10, 0
-        Triple(1, 2, 0).formatted(true).println()       //<10, <10, 0
-        Triple(1, 23, 0).formatted(true).println()      //<10, >10, 0
+        Triple(12, 34, 0).formatted(true).let { assert(it == "12:34:00") }     //>10, >10, 0
+        Triple(12, 3, 0).formatted(true).let { assert(it == "12:03:00") }      //>10, <10, 0
+        Triple(1, 2, 0).formatted(true).let { assert(it == "1:02:00") }       //<10, <10, 0
+        Triple(1, 23, 0).formatted(true).let { assert(it == "1:23:00") }      //<10, >10, 0
 
         //>1, >1, >1
-        Triple(12, 34, 56).formatted(true).println()     //>10, >10, >10
-        Triple(12, 34, 5).formatted(true).println()      //>10, >10, <10
-        Triple(12, 3, 4).formatted(true).println()       //>10, <10, <10
-        Triple(12, 3, 45).formatted(true).println()      //>10, <10, >10
-        Triple(1, 23, 45).formatted(true).println()      //<10, >10, >10
-        Triple(1, 23, 4).formatted(true).println()       //<10, >10, <10
-        Triple(1, 2, 3).formatted(true).println()        //<10, <10, <10
-        Triple(1, 2, 34).formatted(true).println()       //<10, <10, >10
+        Triple(12, 34, 56).formatted(true).let { assert(it == "12:34:56") }     //>10, >10, >10
+        Triple(12, 34, 5).formatted(true).let { assert(it == "12:34:05") }      //>10, >10, <10
+        Triple(12, 3, 4).formatted(true).let { assert(it == "12:03:04") }       //>10, <10, <10
+        Triple(12, 3, 45).formatted(true).let { assert(it == "12:03:45") }      //>10, <10, >10
+        Triple(1, 23, 45).formatted(true).let { assert(it == "1:23:45") }      //<10, >10, >10
+        Triple(1, 23, 4).formatted(true).let { assert(it == "1:23:04") }       //<10, >10, <10
+        Triple(1, 2, 3).formatted(true).let { assert(it == "1:02:03") }        //<10, <10, <10
+        Triple(1, 2, 34).formatted(true).let { assert(it == "1:02:34") }       //<10, <10, >10
     }
 }
